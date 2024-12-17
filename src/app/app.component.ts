7246d08bb00d3bc88203400d2098158c48eff595
TRVevel <piscionequentin@gmail.com>
2024-12-17 17:50:44 +0100

    Finis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { User } from './models/User';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,HeaderComponent, FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'SyntheseApp';
+
+ngOnInit(){
+  let user0:User= new User(0, "Alice", "Alice@gmail.com")
+let user1:User= new User(1, "Ava Sarfez", "AvaSarfez@gmail.com")
+let user2:User= new User(2, "Aaron Sarfez", "AaronSarfez@gmail.com")
+  console.log(user0)
+  console.log(user1)
+  console.log(user2)
+
+}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'users', component: UsersComponent },
+    { path: 'about', component: AboutComponent }
+    
+];
